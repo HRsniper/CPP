e80ce421991746e4e84a037cf4cd27986f60ef9f
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-01-08 01:42:06 -0400

    vector of strings

--- tesseract-ocr_test/main.cpp
+++ tesseract-ocr_test/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <leptonica/allheaders.h>
 #include <opencv4/opencv2/core.hpp>
 #include <opencv4/opencv2/highgui.hpp>
 #include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
 #include <tesseract/baseapi.h>
 
-int main(int argc, char const *argv[]) {
+#include <boost/algorithm/string.hpp>
 
-  std::string image_path = cv::samples::findFile("testopencv.bmp");
+int main(int argc, char const *argv[]) {
+  const char *tessdata_path = "C:/Desenvolvimento/msys64/mingw64/share/tessdata";
+  std::string image_path    = cv::samples::findFile("testopencv.bmp");
 
   cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+  cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
 
   if (img.empty()) {
     std::cout << "Não foi possível ler a imagem: " << image_path << std::endl;
@@ -20,26 +25,37 @@ int main(int argc, char const *argv[]) {
 
   cv::imshow("Aperte qualquer tecla para fechar", img);
 
-  // initilize tesseract OCR engine
+  // inicializando tesseract OCR engine
   tesseract::TessBaseAPI *tesseract = new tesseract::TessBaseAPI();
   // tesseract::TessBaseAPI tesseract;
 
-  tesseract->Init("tessdata", "osd", tesseract::OEM_COUNT);
+  tesseract->Init(tessdata_path, "eng", tesseract::OEM_COUNT);
   tesseract->SetPageSegMode(tesseract::PSM_AUTO);
 
-  tesseract->SetImage(img.data, img.cols, img.rows, 1, img.step);
+  tesseract->SetImage(img.data, img.size().width, img.size().height, img.channels(), img.step);
 
-  std::string outText    = std::string(tesseract->GetUTF8Text());
-  int         confidence = tesseract->MeanTextConf();
+  std::string outText = tesseract->GetUTF8Text();
+  // int         confidence = tesseract->MeanTextConf();
 
-  std::cout << "Text: " << outText << std::endl;
-  std::cout << "Confidence: " << confidence << std::endl;
+  // detectando letras
+  std::string outBox = tesseract->GetBoxText(0);
 
-  // destroy tesseract OCR engine
+  std::cout << "Text: " << outText;
+  // std::cout << "Confidence: " << confidence << std::endl;
+  // std::cout << "Box: " << outBox << std::endl;
+
+  std::vector<std::string> r;
+  boost::split(r, outBox, boost::is_any_of("\n"));
+
+  for (int i = 0; i < r.size() - 1; i++) {
+    std::cout << r[i] << std::endl;
+  }
+
+  // destruindo e limpando tesseract OCR engine
   tesseract->Clear();
   tesseract->End();
 
-  int k = cv::waitKey(0); // Aguardando ate pressionamento de tecla na janela para fechar a janela
+  int key = cv::waitKey(0); // Aguardando ate pressionamento de tecla na janela para fechar a janela
 
   return 0;
 }
