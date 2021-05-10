f979233b431d1c29954cd3d00c2e58d73698b242
Hércules Ramlow (HRsniper) <63937242+HRsniper@users.noreply.github.com>
2021-05-10 14:41:01 -0400

    compile aula17

--- GetCursorPos.cpp
+++ GetCursorPos.cpp
@@ -10,5 +10,7 @@ int main() {
 
   std::cout << cursorPos.x << " " << cursorPos.y << "\n";
 
+  system("pause");
+
   return 0;
 }
---
+++ knn/start_knn.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  system(".\\knn.exe < dataset.txt");
+  system("pause");
+
+  return 0;
+}
